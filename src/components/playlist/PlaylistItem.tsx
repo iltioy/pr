6b49b29d973ddd26dfd332b199667ab8f0229431
iltioy <tema.illar2@gmail.com>
2023-10-30 22:33:38 +0300

    mix songs main page

--- src/components/playlist/PlaylistItem.tsx
+++ src/components/playlist/PlaylistItem.tsx
@@ -11,231 +11,231 @@ import { useStores } from "../../root-store-context";
 import { toggleFavoritePlaylist } from "../../queries/playlists";
 
 interface PlaylistItemIconsProps {
-  playlist: PlaylistType;
+    playlist: PlaylistType;
 }
 
-const PlaylistItemIcons: React.FC<PlaylistItemIconsProps> = observer(
-  ({ playlist }) => {
+const PlaylistItemIcons: React.FC<PlaylistItemIconsProps> = observer(({ playlist }) => {
     const { ref: favIconRef, scale: favIconScale } = useHold({
-      size: 0.9,
-      initialScale: 1.15,
+        size: 0.9,
+        initialScale: 1.15,
     });
 
     const { ref: shareIconRef, scale: shareIconScale } = useHold({
-      size: 0.9,
-      initialScale: 1.15,
+        size: 0.9,
+        initialScale: 1.15,
     });
 
-    const { playlistsStore } = useStores();
+    const { playlistsStore, songsStore } = useStores();
     const { username } = useParams();
 
     const handleToggleFavoritePlaylist = () => {
-      toggleFavoritePlaylist(playlist.id, username);
+        toggleFavoritePlaylist(playlist.id, username);
     };
 
     return (
-      <>
-        <Stack
-          width="100%"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="row"
-          gap="10px"
-        >
-          <Stack
-            sx={{
-              background: "black",
-              opacity: 0.8,
-              width: "42px",
-              height: "42px",
-              display: "flex",
-              borderRadius: "50%",
-              justifyContent: "center",
-              alignItems: "center",
-              transition: "0.3s",
-              ":hover": {
-                transform: `scale(${favIconScale})`,
-              },
-            }}
-            ref={favIconRef}
-          >
-            {playlistsStore.user_liked_playlists_ids?.includes(playlist.id) ? (
-              <FavoriteIcon
-                htmlColor="white"
-                sx={{
-                  width: "25px",
-                  height: "25px",
-                }}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleToggleFavoritePlaylist();
-                }}
-              />
-            ) : (
-              <FavoriteBorderIcon
-                htmlColor="white"
-                sx={{
-                  width: "25px",
-                  height: "25px",
-                }}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  handleToggleFavoritePlaylist();
-                }}
-              />
-            )}
-          </Stack>
-          <Stack
-            sx={{
-              background: "orange",
-              width: "55px",
-              height: "55px",
-              display: "flex",
-              borderRadius: "50%",
-              justifyContent: "center",
-              alignItems: "center",
-              transition: "0.3s",
-              ":hover": {
-                transform: "scale(1.15)",
-              },
-            }}
-          >
-            <PlayArrowIcon
-              sx={{
-                width: "30px",
-                height: "30px",
-              }}
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            />
-          </Stack>
-          <Stack
-            sx={{
-              background: "black",
-              opacity: 0.8,
-              width: "42px",
-              height: "42px",
-              display: "flex",
-              borderRadius: "50%",
-              justifyContent: "center",
-              alignItems: "center",
-              transition: "0.3s",
-              ":hover": {
-                transform: `scale(${shareIconScale})`,
-              },
-            }}
-            ref={shareIconRef}
-          >
-            <IosShareIcon
-              htmlColor="white"
-              sx={{
-                width: "25px",
-                height: "25px",
-              }}
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            />
-          </Stack>
-        </Stack>
-      </>
+        <>
+            <Stack
+                width="100%"
+                justifyContent="center"
+                alignItems="center"
+                flexDirection="row"
+                gap="10px"
+            >
+                <Stack
+                    sx={{
+                        background: "black",
+                        opacity: 0.8,
+                        width: "42px",
+                        height: "42px",
+                        display: "flex",
+                        borderRadius: "50%",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        transition: "0.3s",
+                        ":hover": {
+                            transform: `scale(${favIconScale})`,
+                        },
+                    }}
+                    ref={favIconRef}
+                >
+                    {playlistsStore.user_liked_playlists_ids?.includes(playlist.id) ? (
+                        <FavoriteIcon
+                            htmlColor="white"
+                            sx={{
+                                width: "25px",
+                                height: "25px",
+                            }}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                handleToggleFavoritePlaylist();
+                            }}
+                        />
+                    ) : (
+                        <FavoriteBorderIcon
+                            htmlColor="white"
+                            sx={{
+                                width: "25px",
+                                height: "25px",
+                            }}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                handleToggleFavoritePlaylist();
+                            }}
+                        />
+                    )}
+                </Stack>
+                <Stack
+                    sx={{
+                        background: "orange",
+                        width: "55px",
+                        height: "55px",
+                        display: "flex",
+                        borderRadius: "50%",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        transition: "0.3s",
+                        ":hover": {
+                            transform: "scale(1.15)",
+                        },
+                    }}
+                >
+                    <PlayArrowIcon
+                        sx={{
+                            width: "30px",
+                            height: "30px",
+                        }}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            songsStore.setSongQueue(playlist);
+                            songsStore.shuffleSongQueue();
+                        }}
+                    />
+                </Stack>
+                <Stack
+                    sx={{
+                        background: "black",
+                        opacity: 0.8,
+                        width: "42px",
+                        height: "42px",
+                        display: "flex",
+                        borderRadius: "50%",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        transition: "0.3s",
+                        ":hover": {
+                            transform: `scale(${shareIconScale})`,
+                        },
+                    }}
+                    ref={shareIconRef}
+                >
+                    <IosShareIcon
+                        htmlColor="white"
+                        sx={{
+                            width: "25px",
+                            height: "25px",
+                        }}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                        }}
+                    />
+                </Stack>
+            </Stack>
+        </>
     );
-  }
-);
+});
 
 interface PlaylisyItemProps {
-  playlist: PlaylistType;
+    playlist: PlaylistType;
 }
 
 const PlaylistItem: React.FC<PlaylisyItemProps> = observer(({ playlist }) => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <Stack
-      sx={{
-        height: "290px",
-        width: "230px",
-        borderRadius: "5px",
-        justifyContent: "center",
-        alignItems: "center",
-        position: "relative",
-        zIndex: 1,
-        cursor: "pointer",
-        ":hover .hoverPlaylistItem": {
-          display: "flex",
-        },
-      }}
-      color="text.primary"
-      onClick={() => {
-        navigate(`/${playlist.owner.username}/playlist/${playlist.id}`);
-      }}
-    >
-      <Stack
-        className="hoverPlaylistItem"
-        sx={{
-          position: "absolute",
-          zIndex: 2,
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          display: "none",
-          border: "3px solid orange",
-          justifyContent: "center",
-        }}
-      />
-
-      <Stack
-        sx={{
-          height: "260px",
-          width: "220px",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
+    return (
         <Stack
-          sx={{
-            height: "200px",
-            width: "200px",
-            marginBottom: "5px",
-            position: "relative",
-          }}
-        >
-          <img
-            src={playlist.image.image_url}
-            alt=""
-            style={{
-              objectFit: "cover",
-              height: "100%",
-              width: "100%",
-            }}
-          />
-
-          <Stack
-            className="hoverPlaylistItem"
             sx={{
-              position: "absolute",
-              zIndex: 3,
-              top: 0,
-              bottom: 0,
-              left: 0,
-              right: 0,
-              display: "none",
-              justifyContent: "center",
-              alignItems: "center",
+                height: "290px",
+                width: "230px",
+                borderRadius: "5px",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "relative",
+                zIndex: 1,
+                cursor: "pointer",
+                ":hover .hoverPlaylistItem": {
+                    display: "flex",
+                },
             }}
-          >
-            <PlaylistItemIcons playlist={playlist} />
-          </Stack>
-        </Stack>
+            color="text.primary"
+            onClick={() => {
+                navigate(`/${playlist.owner.username}/playlist/${playlist.id}`);
+            }}
+        >
+            <Stack
+                className="hoverPlaylistItem"
+                sx={{
+                    position: "absolute",
+                    zIndex: 2,
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    display: "none",
+                    border: "3px solid orange",
+                    justifyContent: "center",
+                }}
+            />
 
-        <Typography width="200px" noWrap>
-          {playlist.name}
-        </Typography>
-      </Stack>
-    </Stack>
-  );
+            <Stack
+                sx={{
+                    height: "260px",
+                    width: "220px",
+                    alignItems: "center",
+                    flexDirection: "column",
+                }}
+            >
+                <Stack
+                    sx={{
+                        height: "200px",
+                        width: "200px",
+                        marginBottom: "5px",
+                        position: "relative",
+                    }}
+                >
+                    <img
+                        src={playlist.image.image_url}
+                        alt=""
+                        style={{
+                            objectFit: "cover",
+                            height: "100%",
+                            width: "100%",
+                        }}
+                    />
+
+                    <Stack
+                        className="hoverPlaylistItem"
+                        sx={{
+                            position: "absolute",
+                            zIndex: 3,
+                            top: 0,
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            display: "none",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <PlaylistItemIcons playlist={playlist} />
+                    </Stack>
+                </Stack>
+
+                <Typography width="200px" noWrap>
+                    {playlist.name}
+                </Typography>
+            </Stack>
+        </Stack>
+    );
 });
 
 export default PlaylistItem;
--- src/pages/AllPlaylistsPlage/AllPlaylistsHeader.tsx
+++ src/pages/AllPlaylistsPlage/AllPlaylistsHeader.tsx
@@ -2,106 +2,125 @@ import { Avatar, Stack, Typography, Box } from "@mui/material";
 import { observer } from "mobx-react-lite";
 import { useStores } from "../../root-store-context";
 import PressableButton from "../../components/MixSongsButton";
+import { playlists } from "../../faker";
+import { PlaylistType } from "../../types";
+import * as _ from "lodash";
 
 const AllPlaylistsHeader = observer(() => {
-  const { userStore } = useStores();
+    const { userStore } = useStores();
+    const { songsStore, playlistsStore } = useStores();
 
-  return (
-    <Stack
-      sx={{
-        width: "100%",
-        alignItems: "center",
-        marginBottom: "40px",
-      }}
-      bgcolor="custom.bg.secondary"
-      color="text.primary"
-    >
-      <Stack
-        sx={{
-          width: {
-            xs: "95%",
-            sm: "80%",
-          },
-        }}
-        height="100%"
-        flexDirection="row"
-      >
-        <Avatar
-          src="https://avatars.mds.yandex.net/get-yapic/0/0-0/islands-200"
-          alt="default user"
-          sx={{
-            height: {
-              xs: "125px",
-              md: "200px",
-            },
-            width: {
-              xs: "125px",
-              md: "200px",
-            },
-            marginTop: {
-              xs: "37.5px",
-              md: "50px",
-            },
-            marginBottom: {
-              xs: "37.5px",
-              md: "50px",
-            },
-            objectFit: "cover",
-          }}
-        />
-
+    return (
         <Stack
-          flexDirection="column"
-          paddingLeft="30px"
-          sx={{
-            marginTop: {
-              xs: "37.5px",
-              md: "50px",
-            },
-            overflow: "hidden",
-          }}
+            sx={{
+                width: "100%",
+                alignItems: "center",
+                marginBottom: "40px",
+            }}
+            bgcolor="custom.bg.secondary"
+            color="text.primary"
         >
-          <Typography
-            variant="caption"
-            color="grey"
-            sx={{
-              fontSize: {
-                xs: "17px",
-                md: "27px",
-              },
-            }}
-          >
-            Коллекция
-          </Typography>
+            <Stack
+                sx={{
+                    width: {
+                        xs: "95%",
+                        sm: "80%",
+                    },
+                }}
+                height="100%"
+                flexDirection="row"
+            >
+                <Avatar
+                    src="https://avatars.mds.yandex.net/get-yapic/0/0-0/islands-200"
+                    alt="default user"
+                    sx={{
+                        height: {
+                            xs: "125px",
+                            md: "200px",
+                        },
+                        width: {
+                            xs: "125px",
+                            md: "200px",
+                        },
+                        marginTop: {
+                            xs: "37.5px",
+                            md: "50px",
+                        },
+                        marginBottom: {
+                            xs: "37.5px",
+                            md: "50px",
+                        },
+                        objectFit: "cover",
+                    }}
+                />
 
-          <Typography
-            variant="h4"
-            sx={{
-              fontWeight: "bold",
-              fontSize: {
-                xs: "20px",
-                md: "30px",
-              },
-              marginBottom: {
-                xs: "15px",
-                md: "30px",
-              },
-            }}
-          >
-            {userStore.user.username}
-          </Typography>
+                <Stack
+                    flexDirection="column"
+                    paddingLeft="30px"
+                    sx={{
+                        marginTop: {
+                            xs: "37.5px",
+                            md: "50px",
+                        },
+                        overflow: "hidden",
+                    }}
+                >
+                    <Typography
+                        variant="caption"
+                        color="grey"
+                        sx={{
+                            fontSize: {
+                                xs: "17px",
+                                md: "27px",
+                            },
+                        }}
+                    >
+                        Коллекция
+                    </Typography>
 
-          <Box
-            onClick={() => {
-              window.scrollTo({ top: 0, behavior: "smooth" });
-            }}
-          >
-            <PressableButton text="Перемешать" icon="play" />
-          </Box>
+                    <Typography
+                        variant="h4"
+                        sx={{
+                            fontWeight: "bold",
+                            fontSize: {
+                                xs: "20px",
+                                md: "30px",
+                            },
+                            marginBottom: {
+                                xs: "15px",
+                                md: "30px",
+                            },
+                        }}
+                    >
+                        {userStore.user.username}
+                    </Typography>
+
+                    <Box
+                        onClick={() => {
+                            window.scrollTo({ top: 0, behavior: "smooth" });
+                            songsStore.clearSongQueue();
+
+                            let playlist: PlaylistType = _.cloneDeep(
+                                playlistsStore.added_playlists[0]
+                            );
+
+                            for (let i = 1; i < playlistsStore.added_playlists.length; i++) {
+                                playlist.songs = [
+                                    ...playlist.songs,
+                                    ...playlistsStore.added_playlists[i].songs,
+                                ];
+                            }
+
+                            songsStore.setSongQueue(playlist);
+                            songsStore.shuffleSongQueue();
+                        }}
+                    >
+                        <PressableButton text="Перемешать" icon="play" />
+                    </Box>
+                </Stack>
+            </Stack>
         </Stack>
-      </Stack>
-    </Stack>
-  );
+    );
 });
 
 export default AllPlaylistsHeader;
