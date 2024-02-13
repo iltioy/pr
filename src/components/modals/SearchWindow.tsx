12afc20368ead546f3c526edff4a2641253eae80
iltioy <tema.illar2@gmail.com>
2024-02-13 03:08:18 +0300

    search context

--- src/components/modals/SearchWindow.tsx
+++ src/components/modals/SearchWindow.tsx
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import SongRecord from "../SongRecord";
 import axios from "axios";
-import { SongType } from "../../types";
+import { PlaylistType, SongType, OrderedSongType } from "../../types";
 import SearchIcon from "@mui/icons-material/Search";
 import songsStore from "../../stores/songs-store";
+import { observer } from "mobx-react-lite";
 
 interface SearchWindowProps {
     setIsSearchOpened: React.Dispatch<React.SetStateAction<boolean>>;
@@ -20,229 +21,257 @@ let searchedPages: number[] = [];
 let page: number = 0;
 let isLimitReached: boolean = false;
 
-const SearchWindow: React.FC<SearchWindowProps> = ({ setIsSearchOpened }) => {
-    useEffect(() => {
-        document.body.style.overflow = "hidden";
+let mockPlaylist: PlaylistType = {
+    id: 0,
+    image: {},
+    name: "mock",
+    order: 0,
+    owner: {},
+    songs: [],
+};
 
-        return () => {
-            document.body.style.overflow = "auto";
-        };
-    }, []);
+const SearchWindow: React.FC<SearchWindowProps> = observer(
+    ({ setIsSearchOpened }) => {
+        useEffect(() => {
+            document.body.style.overflow = "hidden";
 
-    const [status, setStatus] = useState<SearchStatus>(SearchStatus.CATALOG);
-    const [searchValue, setSearchValue] = useState("");
-    const [songs, setSongs] = useState<SongType[]>([]);
-    const [delay, setDelay] = useState(1000);
+            return () => {
+                document.body.style.overflow = "auto";
+            };
+        }, []);
 
-    const { ref, inView } = useInView();
+        const [status, setStatus] = useState<SearchStatus>(
+            SearchStatus.CATALOG
+        );
+        const [searchValue, setSearchValue] = useState("");
+        const [songs, setSongs] = useState<SongType[]>([]);
+        const [delay, setDelay] = useState(1000);
+        const [songContext, setSongContext] = useState(mockPlaylist);
 
-    const fetchSongs = async () => {
-        try {
-            if (searchedPages.includes(page)) return;
-            const res = await axios.get(
-                `/songs/search?query=${searchValue}&page=${page}`
-            );
+        const { ref, inView } = useInView();
 
-            const data: SongType[] = res.data;
+        const fetchSongs = async () => {
+            try {
+                if (searchedPages.includes(page)) return;
+                const res = await axios.get(
+                    `/songs/search?query=${searchValue}&page=${page}`
+                );
 
-            setSongs((prevData) => {
-                return [...prevData, ...data];
-            });
-            searchedPages.push(page);
+                const data: SongType[] = res.data;
 
-            if (data?.length === 0) {
-                isLimitReached = true;
+                setSongs((prevData) => {
+                    return [...prevData, ...data];
+                });
+                searchedPages.push(page);
+
+                if (data?.length === 0) {
+                    isLimitReached = true;
+                }
+
+                createSongCotext();
+            } catch (error) {
+                console.log(error);
             }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    const searchAddedSongs = () => {
-        setSongs(songsStore.searchSongs(searchValue));
-        isLimitReached = true;
-    };
-
-    const changePage = () => {
-        page++;
-        console.log(status);
-        if (status === SearchStatus.CATALOG) {
-            fetchSongs();
-        } else if (status === SearchStatus.MY_MUSIC) {
-            searchAddedSongs();
-        }
-    };
-
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            searchedPages = [];
-            page = 0;
-            isLimitReached = false;
-            setSongs([]);
-            changePage();
-        }, delay);
-
-        return () => {
-            clearTimeout(timeoutId);
-            searchedPages = [];
-            page = 0;
-            isLimitReached = false;
         };
-    }, [searchValue, status, delay]);
 
-    useEffect(() => {
-        if (inView && songs.length > 0 && !isLimitReached) {
-            changePage();
-        }
-    }, [inView, songs]);
+        const searchAddedSongs = () => {
+            setSongs(songsStore.searchSongs(searchValue));
+            createSongCotext();
+            isLimitReached = true;
+        };
 
-    return (
-        <>
-            <Stack
-                position="absolute"
-                color="text.primary"
-                // bottom="0px"
-                width="100%"
-                height="100%"
-                top="0"
-                bottom="0"
-                left="0"
-                right="0"
-                bgcolor="rgb(0,0,0)"
-                zIndex={10}
-                overflow="hidden"
-                sx={{
-                    opacity: 0.7,
-                }}
-            />
-            <Stack
-                position="absolute"
-                width="100%"
-                height="100%"
-                zIndex={12}
-                alignItems="center"
-                sx={{
-                    opacity: "1",
-                }}
-                overflow="auto"
-                onClick={() => {
-                    setIsSearchOpened(false);
-                }}
-                className="searchWindowScroll"
-            >
-                <Stack width="80%" marginTop="12px">
+        const changePage = () => {
+            page++;
+            if (status === SearchStatus.CATALOG) {
+                fetchSongs();
+            } else if (status === SearchStatus.MY_MUSIC) {
+                searchAddedSongs();
+            }
+        };
+
+        const createSongCotext = () => {
+            setSongContext((prevState) => {
+                let orderedSongs: OrderedSongType[] = [];
+                songs.forEach((song) => orderedSongs.push({ song, order: 1 }));
+                return {
+                    ...prevState,
+                    songs: orderedSongs,
+                };
+            });
+        };
+
+        useEffect(() => {
+            const timeoutId = setTimeout(() => {
+                searchedPages = [];
+                page = 0;
+                isLimitReached = false;
+                setSongs([]);
+                changePage();
+            }, delay);
+
+            return () => {
+                clearTimeout(timeoutId);
+                searchedPages = [];
+                page = 0;
+                isLimitReached = false;
+            };
+        }, [searchValue, status, delay]);
+
+        useEffect(() => {
+            if (inView && songs.length > 0 && !isLimitReached) {
+                changePage();
+            }
+        }, [inView, songs]);
+
+        return (
+            <>
+                <Stack
+                    position="absolute"
+                    color="text.primary"
+                    // bottom="0px"
+                    width="100%"
+                    height="100%"
+                    top="0"
+                    bottom="0"
+                    left="0"
+                    right="0"
+                    bgcolor="rgb(0,0,0)"
+                    zIndex={10}
+                    overflow="hidden"
+                    sx={{
+                        opacity: 0.7,
+                    }}
+                />
+                <Stack
+                    position="absolute"
+                    width="100%"
+                    height="100%"
+                    zIndex={12}
+                    alignItems="center"
+                    sx={{
+                        opacity: "1",
+                    }}
+                    overflow="auto"
+                    onClick={() => {
+                        setIsSearchOpened(false);
+                    }}
+                    className="searchWindowScroll"
+                >
+                    <Stack width="80%" marginTop="12px">
+                        <Stack
+                            flexDirection="row"
+                            alignItems="center"
+                            p="4px"
+                            borderBottom={{
+                                sm: `1px solid #ffffff`,
+                                xs: "none",
+                                zIndex: 11,
+                            }}
+                            onClick={(e) => {
+                                e.stopPropagation();
+                            }}
+                        >
+                            <SearchIcon htmlColor={"#ffffff"} />
+                            <InputBase
+                                sx={{
+                                    paddingLeft: "5px",
+                                    width: "100%",
+                                    display: {
+                                        xs: "block",
+                                        sm: "block",
+                                    },
+                                    color: "#ffffff",
+                                }}
+                                placeholder="Посик..."
+                                autoFocus
+                                value={searchValue}
+                                onChange={(e) => {
+                                    setSearchValue(e.target.value);
+                                }}
+                            />
+                        </Stack>
+                        <Stack
+                            marginTop="60px"
+                            flexDirection="row"
+                            color="white"
+                            width="100%"
+                            justifyContent="center"
+                            gap="50px"
+                        >
+                            <Button
+                                color={
+                                    status === SearchStatus.CATALOG
+                                        ? "primary"
+                                        : "inherit"
+                                }
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    setDelay(1000);
+                                    setStatus(SearchStatus.CATALOG);
+                                }}
+                            >
+                                Каталог
+                            </Button>
+                            <Button
+                                color={
+                                    status === SearchStatus.MY_MUSIC
+                                        ? "primary"
+                                        : "inherit"
+                                }
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    setDelay(200);
+                                    setStatus(SearchStatus.MY_MUSIC);
+                                }}
+                            >
+                                Моя музыка
+                            </Button>
+                        </Stack>
+                    </Stack>
+
                     <Stack
-                        flexDirection="row"
-                        alignItems="center"
-                        p="4px"
-                        borderBottom={{
-                            sm: `1px solid #ffffff`,
-                            xs: "none",
-                            zIndex: 11,
+                        sx={{
+                            width: {
+                                xs: "95%",
+                                sm: "70%",
+                            },
                         }}
+                        color="white"
+                        marginTop="20px"
                         onClick={(e) => {
                             e.stopPropagation();
                         }}
+                        paddingBottom="50px"
                     >
-                        <SearchIcon htmlColor={"#ffffff"} />
-                        <InputBase
-                            sx={{
-                                paddingLeft: "5px",
-                                width: "100%",
-                                display: {
-                                    xs: "block",
-                                    sm: "block",
-                                },
-                                color: "#ffffff",
-                            }}
-                            placeholder="Посик..."
-                            autoFocus
-                            value={searchValue}
-                            onChange={(e) => {
-                                setSearchValue(e.target.value);
-                            }}
-                        />
-                    </Stack>
-                    <Stack
-                        marginTop="60px"
-                        flexDirection="row"
-                        color="white"
-                        width="100%"
-                        justifyContent="center"
-                        gap="50px"
-                    >
-                        <Button
-                            color={
-                                status === SearchStatus.CATALOG
-                                    ? "primary"
-                                    : "inherit"
-                            }
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setDelay(1000);
-                                setStatus(SearchStatus.CATALOG);
-                            }}
-                        >
-                            Каталог
-                        </Button>
-                        <Button
-                            color={
-                                status === SearchStatus.MY_MUSIC
-                                    ? "primary"
-                                    : "inherit"
-                            }
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setDelay(200);
-                                setStatus(SearchStatus.MY_MUSIC);
-                            }}
-                        >
-                            Моя музыка
-                        </Button>
-                    </Stack>
-                </Stack>
+                        {songs?.map((song: SongType, index: number) => {
+                            return (
+                                <SongRecord
+                                    key={index}
+                                    song={song}
+                                    sx={{
+                                        backgroundColor: "rgba(0,0,0,0.5)",
+                                        border: "none",
+                                    }}
+                                    songContext={songContext}
+                                    search
+                                />
+                            );
+                        })}
+                        <span ref={ref}></span>
 
-                <Stack
-                    sx={{
-                        width: {
-                            xs: "95%",
-                            sm: "70%",
-                        },
-                    }}
-                    color="white"
-                    marginTop="20px"
-                    onClick={(e) => {
-                        e.stopPropagation();
-                    }}
-                    paddingBottom="50px"
-                >
-                    {songs?.map((song: SongType, index: number) => {
-                        return (
-                            <SongRecord
-                                key={index}
-                                song={song}
-                                sx={{
-                                    backgroundColor: "rgba(0,0,0,0.5)",
-                                    border: "none",
-                                }}
-                                search
-                            />
-                        );
-                    })}
-                    <span ref={ref}></span>
-
-                    {/* <Button onClick={() => fetchSongs()}>fetchSongs</Button> */}
-                    {/* <Button
+                        {/* <Button onClick={() => fetchSongs()}>fetchSongs</Button> */}
+                        {/* <Button
             onClick={() => {
               changePage();
             }}
           >
             more
           </Button> */}
+                    </Stack>
                 </Stack>
-            </Stack>
-        </>
-    );
-};
+            </>
+        );
+    }
+);
 
 export default SearchWindow;
